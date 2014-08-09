75682128154833deef72eb9bcb2f60a69d98d2ad
freakr <olaf.roenning@gmx.de>
2014-08-09 21:58:31 +0200

    V0.013 *Added Constants for SQL* *Changed create_table in DataBase*

--- WDD_Server/src/freakrware/wdd/server/resources/WDD_interface.java
+++ WDD_Server/src/freakrware/wdd/server/resources/WDD_interface.java
@@ -20,11 +20,29 @@ public interface WDD_interface {
 	public static final String HOST = "freakr.spdns.de";
 	public static final int PORT = 15000;
 	public static final int THREAD_POOL_COUNT = 20;
-	// Datenbank 
+	// DataBase Basic Parameter 
 	public static final String DB_DRIVER = "org.hsqldb.jdbcDriver";
 	public static final String DB_FOLDER = "WDDData/";
 	public static final String DB_NAME = "DBWDD";
-	// Sonstige
+	// DataBase Getter
+	final static String[] GETTER_USER_EXISTS = {"UserID"};
+	// DataBase Table-Names
+	final static String DB_TABLE_USERNAME = "Username";
+	final static String DB_TABLE_USERDATA = "Userdata";
+	final static String DB_TABLE_MESSAGES = "Messages";
+	final static String DB_TABLE_MESSAGEBOARD = "Messageboard";
+	// DataBase Columns
+	final static String DB_COL_USERNAME = "Username";
+	final static String DB_COL_USERID = "UserID";
+	final static String DB_COL_USERIP = "UserIP";
+	final static String DB_COL_MESSAGEID = "MessageID";
+	final static String DB_COL_MESSAGETEXT = "Messagetext";
+	final static String DB_COL_ACTIONID = "ActionID";
+	final static String DB_COL_SENTFROMUSERID = "SentFromUserID";
+	final static String DB_COL_SENTTOUSERID = "SentToUserID";
+	final static String DB_COL_SENTTRUE = "SentTrue";
+	final static String DB_COL_INCOMINGDATE = "IncomingDate";
+	// Others
 	public final static String SETUP = "Setup";
 	// Client Parameter
 	static final String ONLINE_THREAD = "Online_Thread";
@@ -36,5 +54,6 @@ public interface WDD_interface {
 	final static String SERVERSTATUS = "Serverstatus";
 	final static String SERVERSTATUS_ON = "A";
 	final static String SERVERSTATUS_OFF = "I";
+	
 	
 }
