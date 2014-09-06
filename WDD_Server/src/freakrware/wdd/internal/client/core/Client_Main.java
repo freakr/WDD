11b13e54beafbae128723b959323e721fa9b2581
freakr <olaf.roenning@gmx.de>
2014-09-06 16:28:59 +0200

    V0.029 *removed Datenbank.java - not necessary anymore*

--- WDD_Server/src/freakrware/wdd/internal/client/core/Client_Main.java
+++ WDD_Server/src/freakrware/wdd/internal/client/core/Client_Main.java
@@ -1,14 +1,15 @@
 package freakrware.wdd.internal.client.core;
 
+import freakrware.wdd.server.resources.SFCP_interface;
 import freakrware.wdd.server.resources.Server_Setup;
 import freakrware.wdd.server.resources.WDD_interface;
 
-public class Client_Main implements WDD_interface{
+public class Client_Main implements WDD_interface,SFCP_interface{
 	
 	public static void main(String[] args) {
 		Server_Setup setup = new Server_Setup();
 		//connection_keep();
-		//new Thread(new Client(setup,LOCALHOST,REQUEST_ADD_MESSAGE, new String[] {"FreakR","FreakR1","TEST 5"})).start();
+		//new Thread(new Client(setup,LOCALHOST,REQUEST_ADD_MESSAGE, new String[] {"FreakR","FreakR1","TEST 3"})).start();
 		//new Thread(new Client(setup,LOCALHOST,REQUEST_NEW_MESSAGES_FROM_BOARD, new String[] {"FreakR1"})).start();
 		new Thread(new Client(setup,LOCALHOST,ONLINESTATUS, new String[] {"FreakR1",ONLINESTATUS_OFF,LOCALHOST})).start();
 		//new Thread(new Client(setup,LOCALHOST,REQUEST_ADD_USER, new String[] {"FreakR1"})).start();
